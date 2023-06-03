7345377476fd06472a77ad7d06312cfad448acb1
gsantosbruna <118274503+gsantosbruna@users.noreply.github.com>
2023-06-03 17:27:38 +0200

    add new languages

--- conectResume.js
+++ conectResume.js
@@ -1,28 +1,28 @@
-// function getUrlParameter(name) {
-//   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-//   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-//   var results = regex.exec(window.location.search);
-//   return results === null
-//     ? ""
-//     : decodeURIComponent(results[1].replace(/\+/g, " "));
-// }
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(window.location.search);
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
-// var jsonLink = "resume";
-// var language = getUrlParameter("lang");
+var jsonLink = "resume";
+var language = getUrlParameter("lang");
 
-// switch (language) {
-//   case "en":
-//     jsonLink = "resume";
-//     break;
-//   case "pt-br":
-//     jsonLink = "curriculo";
-//     break;
-//   case "fr":
-//     jsonLink = "cv";
-//     break;
-// }
+switch (language) {
+  case "en":
+    jsonLink = "resume";
+    break;
+  case "pt-br":
+    jsonLink = "curriculo";
+    break;
+  case "fr":
+    jsonLink = "cv";
+    break;
+}
 
-const url = `https://raw.githubusercontent.com/gsantosbruna/portfolio/main/resume/resume.json`;
+const url = `https://raw.githubusercontent.com/gsantosbruna/portfolio/main/resume/${jsonLink}.json`;
 
 async function getResume() {
   try {
